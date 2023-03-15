46da7f3f1d615445422bbd5e23b7d134e144d0ff
Alferavil88 <alferavil88@gmail.com>
2023-03-15 18:47:59 -0500

    feat: add desktop navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,129 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import defaultAvatar from "../../assets/avatar-default.jpg";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState(false);
+
+  const [open, setOpen] = useState(false);
+
+  const { pathname } = useLocation();
+
+  //mock user
+  /* const user = {
+    id: 1,
+    username: "Alberto",
+    isSeller: true,
+    img: "",
+  };
+ */
+
+  const user = false;
+
+  //Detecting scroll
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  ///////////////////////////////////////////
+
+  // Create a Cloudinary instance and set your cloud name.
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "dk3psx2kr",
+    },
+  });
+
+  const myImage = cld.image(user?.img);
+  myImage.resize(
+    thumbnail().width(150).height(150).gravity(focusOn(FocusOn.face()))
+  );
+
+  /////////////////////////////////////////////////////////////
+
+  return (
+    <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="text">TopTalent</span>
+          </Link>
+        </div>
+        <div className="links">
+          <Link to="/gigs">
+            <span>Explore</span>
+          </Link>
+          <Link to="/login">
+            <span>Sign in</span>
+          </Link>
+          {!user?.isSeller && <span>Become a Seller</span>}
+          {user ? (
+            <div className="user" onClick={() => setOpen((prev) => !prev)}>
+              <div className="info">
+                {!user?.img ? (
+                  <img src={defaultAvatar} alt="avatar" />
+                ) : (
+                  <AdvancedImage cldImg={myImage} />
+                )}
+                <span>{user?.username}</span>
+              </div>
+              {open && (
+                <div className="options">
+                  {user?.isSeller && (
+                    <>
+                      <Link to="/mygigs">
+                        <span>Gigs</span>
+                      </Link>
+                      <Link to="/add">
+                        <span>Add New Gig</span>
+                      </Link>
+                    </>
+                  )}
+                  <Link to="/orders">
+                    <span>Orders</span>
+                  </Link>
+                  <Link to="/messages">
+                    <span>Messages</span>
+                  </Link>
+                  <span>Log out</span>
+                </div>
+              )}
+            </div>
+          ) : (
+            <Link to="/register">
+              <button>Join</button>
+            </Link>
+          )}
+        </div>
+      </div>
+      {(active || pathname !== "/") && (
+        <>
+          <hr />
+          <div className="menu">
+            <Link to="/">Graphics & Design</Link>
+            <Link to="/">Video & Animation</Link>
+            <Link to="/">Writing & Translation</Link>
+            <Link to="/">AI Services</Link>
+            <Link to="/">Digital Marketing</Link>
+            <Link to="/">Music & Audio</Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 export default Navbar;
