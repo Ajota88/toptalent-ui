9afdb9bc6e1f0a0cdb4a1c75dbcb4eb3ea119509
Alferavil88 <alferavil88@gmail.com>
2023-03-15 13:12:28 -0500

    feat: add  react-router-dom configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,90 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import Navbar from "./components/navbar/Navbar";
+import MobileNavbar from "./components/mobileNavbar/MobileNavbar";
+import Footer from "./components/footer/Footer";
+import {
+  AddGig,
+  ErrorPage,
+  GigPage,
+  Gigs,
+  Home,
+  Login,
+  MyGigs,
+  Orders,
+  Payment,
+  Register,
+  Success,
+} from "./pages";
+
 const App = () => {
-  return <div>App</div>;
+  const isMobile = useMediaQuery({ query: "(max-width: 700px)" });
+
+  const Layout = () => {
+    return (
+      <div className="layout">
+        {isMobile ? <MobileNavbar /> : <Navbar />}
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/gigs",
+          element: <Gigs />,
+        },
+        {
+          path: "/gig/:id",
+          element: <GigPage />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+        {
+          path: "/mygigs",
+          element: <MyGigs />,
+        },
+        {
+          path: "/add",
+          element: <AddGig />,
+        },
+
+        {
+          path: "/register",
+          element: <Register />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/payment/:id",
+          element: <Payment />,
+        },
+        {
+          path: "/success",
+          element: <Success />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
 };
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import AddGig from "./addGig/AddGig";
+import ErrorPage from "./errorPage/ErrorPage";
+import GigPage from "./gigPage/GigPage";
+import Gigs from "./gigs/Gigs";
+import Home from "./home/Home";
+import Login from "./login/Login";
+import MyGigs from "./myGigs/MyGigs";
+import Orders from "./orders/Orders";
+import Payment from "./payment/Payment";
+import Register from "./register/Register";
+import Success from "./success/Success";
+
+export {
+  AddGig,
+  ErrorPage,
+  GigPage,
+  Gigs,
+  Home,
+  Login,
+  MyGigs,
+  Orders,
+  Payment,
+  Register,
+  Success,
+};
