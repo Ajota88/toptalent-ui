d6d02b319bed4cb0d03c097dcff9252134502e48
Alferavil88 <alferavil88@gmail.com>
2023-03-15 21:48:44 -0500

    feat: add gigs list page

--- src/components/gigCard/GigCard.jsx
+++ src/components/gigCard/GigCard.jsx
@@ -1,6 +1,68 @@
+import { faStar, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { AdvancedImage } from "@cloudinary/react";
+import { Cloudinary, CloudConfig, CloudinaryImage } from "@cloudinary/url-gen";
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import coverPlaceholder from "../../assets/No_Image_Available.jpg";
+import avatarPlaceholder from "../../assets/avatar-default.jpg";
 import "./GigCard.scss";
 
-const GigCard = () => {
-  return <div>GigCard</div>;
+const GigCard = ({ item }) => {
+  // Create a Cloudinary instance and set your cloud name.
+  let cloudConfig = new CloudConfig({ cloudName: "dk3psx2kr" });
+  const myImage = new CloudinaryImage(item?.img, cloudConfig);
+  myImage.resize(
+    thumbnail().width(150).height(150).gravity(focusOn(FocusOn.face()))
+  );
+
+  const myCover = new CloudinaryImage(item?.cover, cloudConfig);
+
+  /////////////////////////////////////////////////////////////
+
+  return (
+    <div className="gig-card">
+      <Link to={`/gig/${item.id}`}>
+        {!item?.cover ? (
+          <img src={coverPlaceholder} alt="" />
+        ) : (
+          <AdvancedImage cldImg={myCover} />
+        )}
+        {/*  <img
+          src="https://res.cloudinary.com/dk3psx2kr/image/upload/v1/avatars_demo/ayw6jmovom3bqiqsuxcg"
+          alt=""
+        /> */}
+        <div className="info">
+          <div className="user">
+            {!item?.img ? (
+              <img src={avatarPlaceholder} alt="" />
+            ) : (
+              <AdvancedImage cldImg={myImage} />
+            )}
+            <span>{item.username}</span>
+          </div>
+          <p>{item.desc}</p>
+          <div className="star">
+            <FontAwesomeIcon icon={faStar} />
+            <span>
+              {!item.totalReviews
+                ? "No reviews"
+                : Math.round(item.totalStars / item.totalReviews) || 5}
+            </span>
+          </div>
+        </div>
+        <hr />
+        <div className="details">
+          <FontAwesomeIcon icon={faHeart} />
+          <div className="price">
+            <span>STARTING AT</span>
+            <h2>${item.price || 50}</h2>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 export default GigCard;
--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocation } from "react-router-dom";
 import { useGetGigsQuery } from "../../features/gigs/gigsSlice";
+import GigCard from "../../components/gigCard/GigCard";
+//import { gigs } from "../../mockData";
 import "./Gigs.scss";
 
 const Gigs = () => {
+  const [open, setOpen] = useState(false);
+  const [sort, setSort] = useState("sales");
+
+  const { search } = useLocation();
+
   const {
     data: gigs,
     isLoading,
@@ -10,8 +21,59 @@ const Gigs = () => {
     error,
   } = useGetGigsQuery();
 
-  console.log(gigs);
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  };
 
-  return <div>Gigs</div>;
+  return (
+    <div className="gigs">
+      <div className="container">
+        <span className="breadcrumbs">Fiverr {">"} Graphics & Designs</span>
+        <h2>AI Artists</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, est?
+        </p>
+        <div className="menu">
+          <div className="left">
+            <span>Budget</span>
+            <input type="text" placeholder="min price" />
+            <input type="text" placeholder="max price" />
+            <button>Apply</button>
+          </div>
+          <div className="right">
+            <span className="sort-by">Sort By</span>
+            <span className="sort-type">
+              {sort === "sales" ? " Best Selling" : "Newest"}
+            </span>
+            <FontAwesomeIcon
+              icon={faCaretDown}
+              onClick={() => setOpen((prev) => !prev)}
+            />
+            {open && (
+              <div className="right-menu">
+                {sort === "sales" ? (
+                  <span onClick={() => reSort("createdAt")}>Newest</span>
+                ) : (
+                  <span onClick={() => reSort("sales")}>Best Selling</span>
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : isError ? (
+          <h2>Something went wrong</h2>
+        ) : (
+          <div className="cards">
+            {gigs?.map((gig) => (
+              <GigCard key={gig.id} item={gig} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 export default Gigs;
