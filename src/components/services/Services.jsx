e75498be9ea47092a909424a5689346a4a3d5bce
Alferavil88 <alferavil88@gmail.com>
2023-03-15 21:02:05 -0500

    feat: add company services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,62 @@
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Services.scss";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services">
+      <div className="container">
+        <div className="left">
+          <h2>A Whole World of Freelance At Your Hands</h2>
+          <div className="service">
+            <div className="title">
+              <FontAwesomeIcon icon={faCheck} />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              eum.
+            </p>
+          </div>
+          <div className="service">
+            <div className="title">
+              <FontAwesomeIcon icon={faCheck} />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              eum.
+            </p>
+          </div>
+          <div className="service">
+            <div className="title">
+              <FontAwesomeIcon icon={faCheck} />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              eum.
+            </p>
+          </div>
+          <div className="service">
+            <div className="title">
+              <FontAwesomeIcon icon={faCheck} />
+              <h3>Lorem ipsum dolor sit amet.</h3>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem,
+              eum.
+            </p>
+          </div>
+        </div>
+        <div className="right">
+          <img
+            src="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="handshake"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Services;
