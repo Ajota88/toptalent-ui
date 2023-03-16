76ee4347aef34472b27639ec7cb83a9e7a94bfcb
Alferavil88 <alferavil88@gmail.com>
2023-03-15 21:10:57 -0500

    feat: add explore categories component

--- src/components/exploreCategories/ExploreCategories.jsx
+++ src/components/exploreCategories/ExploreCategories.jsx
@@ -1,6 +1,78 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPaintBrush,
+  faAd,
+  faPenToSquare,
+  faVideoCamera,
+  faMusic,
+  faBusinessTime,
+  faMugHot,
+  faChartBar,
+  faComputer,
+  faCamera,
+} from "@fortawesome/free-solid-svg-icons";
 import "./ExploreCategories.scss";
 
 const ExploreCategories = () => {
-  return <div>ExploreCategories</div>;
+  return (
+    <div className="explore">
+      <div className="container">
+        <h1>Explore the marketplace</h1>
+        <div className="items">
+          <div className="item">
+            <FontAwesomeIcon icon={faPaintBrush} />
+            <div className="line"></div>
+            <span>Graphics & Design</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faAd} />
+            <div className="line"></div>
+
+            <span>Digital Marketing</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faPenToSquare} />
+            <div className="line"></div>
+            <span>Writing & Translation</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faVideoCamera} />
+            <div className="line"></div>
+            <span>Video & Animation</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faMusic} />
+            <div className="line"></div>
+            <span>Music & Audio</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faComputer} />
+            <div className="line"></div>
+            <span>Programming & Tech</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faBusinessTime} />
+            <div className="line"></div>
+            <span>Business</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faMugHot} />
+            <div className="line"></div>
+            <span>Lifestyle</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faChartBar} />
+            <div className="line"></div>
+            <span>Data</span>
+          </div>
+          <div className="item">
+            <FontAwesomeIcon icon={faCamera} />
+            <div className="line"></div>
+            <span>Photography</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ExploreCategories;
