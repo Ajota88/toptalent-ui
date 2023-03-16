f290e0323fa42d3e609a3f37a855440a5d74bfb9
Alferavil88 <alferavil88@gmail.com>
2023-03-15 21:14:44 -0500

    feat: add footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faGoogle,
+  faTwitter,
+  faPinterest,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const goToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="logo">
+          <Link to="/" onClick={goToTop}>
+            <h2>TopTotal</h2>
+          </Link>
+          <span> Copyright {new Date().getFullYear()}</span>
+        </div>
+        <div className="socials">
+          <FontAwesomeIcon icon={faFacebook} />
+          <FontAwesomeIcon icon={faGoogle} />
+          <FontAwesomeIcon icon={faTwitter} />
+          <FontAwesomeIcon icon={faInstagram} />
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
